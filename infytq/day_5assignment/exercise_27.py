a3f38f677afd39535db6f1093715332b28cbcca3
amrit <amrit@LUCKY-DESKTOP>
2020-04-20 12:50:44 +0530

    seventh commit

---
+++ infytq/day_5assignment/exercise_27.py
@@ -0,0 +1,51 @@
+'''
+Created on Apr 20, 2020
+
+@author: amrit
+'''
+def withdraw_transcation(amount,value):
+    balance=balance_list[value]
+    new_balance=balance-amount
+    if(new_balance >= 500):
+        balance_list[value]=new_balance
+        print("Transaction completed successfully")
+        print("Balance Amount :", new_balance)
+    else:
+        print("Insufficient Balance")
+    
+def deposit_transcation(amount,value):
+        balance=balance_list[value]
+        new_balance=balance+amount
+        balance_list[value]=new_balance
+        print("Transaction completed successfully")
+        print("Balance Amount :", new_balance)
+        
+def balance_enquiry(value):
+    balance=balance_list[value]
+    print("Balance Amount :", balance)
+    
+def validate_account_number_and_transcation_type():
+    flag=None 
+    value=0
+    for index in range(0,len(account_list)):
+        if(account_list[index]==account_number):
+            flag=True
+            value=index
+            break
+    if flag==True:
+        if(transaction_type=="Withdraw"):
+            withdraw_transcation(amount,value)
+        elif(transaction_type=="Deposit"):
+            deposit_transcation(amount,value)
+        elif(transaction_type=="Balance Enquiry"):
+            balance_enquiry(value)
+        else:
+            print("Invalid Transaction Type")
+            
+account_list=[1001,1002,1003,1004,1005]
+balance_list=[2500,10000,7000,1500,500]
+amount=2000
+account_number=1003
+transaction_type="Withdraw"
+
+validate_account_number_and_transcation_type()
---
+++ infytq/day_6assignment/exercise_30.py
@@ -0,0 +1,115 @@
+'''
+Created on Apr 20, 2020
+
+@author: amrit
+'''
+# Below code passess all test cases
+
+def find_average(mark_list):
+    total=0
+    try:
+        for i in range(0, len(mark_list)):
+            total+=mark_list[i]
+        marks_avg=total/len(mark_list)
+        return marks_avg
+    except TypeError:
+        print("Wrong data type")
+    except NameError:
+        print("Name Error")
+    except ZeroDivisionError:
+        print("ZeroDivisionError")
+    except IndexError:
+        print("index our of range")
+
+try:
+    m_list=[15,26,34,24]
+    print("Average marks:", find_average(m_list))
+except NameError:
+    print("Name Error from main")
+except TypeError:
+    print("Type error from main")
+except ValueError:
+    print("value error from main")
+'''
+
+# statment 3 case 2
+#PF-Exer-30
+
+def find_average(mark_list):
+    total=0
+    try:
+        for i in range(0, len(mark_list)):
+            total+=mark_list[i]
+        marks_avg=total/len(mark_list)
+        return marks_avg
+    except ZeroDivisionError:
+        print("Zero ZeroDivisionError")
+    except TypeError:
+        print("Type Error")
+    except IndexError:
+        print("Index Error")
+try:
+    m4="A"
+    m4=int("A")
+    m_list=[1,2,3,m4]
+    print("Average marks:", find_average(m_list))
+except:
+    print("Some Error")
+
+#Statement 3 case 3
+
+#PF-Exer-30
+
+def find_average(mark_list):
+    total=0
+    try:
+        for i in range(0, len(mark_list)):
+            total+=mark_list[i]
+        marks_avg=total/len(mark_list)
+        return marks_avg
+    except TypeError:
+        print("Wrong data type")
+    except NameError:
+        print("Name Error")
+
+try:
+    mark1="A"
+    mark1=int("A")
+    m_list=[mark1,2,3,4]
+    print("Average marks:", find_average(m_list))
+except NameError:
+    print("Name Error from main")
+except TypeError:
+    print("Type error from main")
+except ValueError:
+    print("value error from main")
+
+# statement 3 case 3
+#PF-Exer-30
+
+def find_average(mark_list):
+    total=0
+    try:
+        for i in range(0, len(mark_list)+1):
+            total+=mark_list[i]
+        marks_avg=total/len(mark_list)
+        return marks_avg
+    except TypeError:
+        print("Wrong data type")
+    except NameError:
+        print("Name Error")
+    except ZeroDivisionError:
+        print("ZeroDivisionError")
+    except IndexError:
+        print("index our of range")
+
+try:
+    m_list=[]
+    print("Average marks:", find_average(m_list))
+except NameError:
+    print("Name Error from main")
+except TypeError:
+    print("Type error from main")
+except ValueError:
+    print("value error from main")
+'''
